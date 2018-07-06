dfc2cf3ca684480e8d5d8b63c7ce1e388e8cfcf2
olegtempl <linner_952@mail.ru>
2018-07-06 23:45:40 +0200

    update the gui tests spec & add shell commands for they

--- configs/shell_commands/serverSelenium.js
+++ configs/shell_commands/serverSelenium.js
@@ -3,6 +3,9 @@
  * @property { string } открытие новой вкладки в консоли ( для OC Linux Mint xfce)
  * @property { string } старт selenium  сервера
  */
-const seleniumServerCommand = ['xfce4-terminal --tab','java -jar -Dwebdriver.gecko.driver=./configs/geckodriver/geckodriver ./configs/geckodriver/selenium_server.jar ;'];
+const seleniumServerCommand = [
+  'xfce4-terminal --tab ',
+  'java -jar -Dwebdriver.gecko.driver=./configs/geckodriver/geckodriver ./configs/geckodriver/selenium_server.jar ;'
+];
 
 module.exports = seleniumServerCommand;
---
+++ test/gui/spec_screenshots.js
@@ -0,0 +1,14 @@
+const cl = require('node-cl-log');
+
+const takeSreenshots = require('./screenshots/createScreenshots');
+
+cl.log('');
+cl.log('Start create the screenshot site!');
+cl.log('');
+
+takeSreenshots
+
+
+cl.log('');
+cl.gre('End create the screenshot site!');
+cl.log('');
