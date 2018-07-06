228c90bd305df600d1f76f038359be4bd53cda6e
olegtempl <linner_952@mail.ru>
2018-07-07 00:08:46 +0200

    add bad gen to the test for fill forms

--- test/gui/spec_userstory.js
+++ test/gui/spec_userstory.js
@@ -2,13 +2,14 @@ const cl = require('node-cl-log');
 
 
 const getGoogleHeading = require('./story/getGoogleHeading');
+// const fillinField = require('./story/fillinField');
 
 cl.log('');
 cl.log('Start the user story tests!');
 cl.log('');
 
 getGoogleHeading
-
+// fillinField
 
 cl.log('');
 cl.gre('End the user story tests!');
